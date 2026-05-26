c5c8ff0353021bbece0535671c9693988c090a5b
aknxad <alikhajoueinejad@gmail.com>
2026-05-26 21:53:39 +0330

    clean up examples

--- crates/limiter_core/examples/demo.rs
+++ crates/limiter_core/examples/demo.rs
@@ -1,5 +1,18 @@
+use limiter_core::{algorithms, limiter::RateLimiter};
+use limiter_storage::{memory::memory_store::MemoryStore, store::QueryDatabase};
+
 fn main() {
-    limiter_core::debug();
+
+      let user_bucket = algorithms::token_bucket::TokenBucket::new_default_value();
+
+    let bucket_id: String = "1".to_string();
+    let db = MemoryStore::new();
+
+    let result = user_bucket.check_rate(bucket_id.clone(), 5, &db);
+
+    println!("result of request is {}", result);
+    let bs = db.find(&bucket_id).unwrap();
+    println!("bucket after update => {:?}", bs);
 }
 
 // cargo run --example demo -p limiter-core
--- crates/limiter_core/src/lib.rs
+++ crates/limiter_core/src/lib.rs
@@ -1,40 +1,6 @@
-mod algorithms;
+pub mod algorithms;
 pub mod limiter;
-mod models;
-mod types;
-mod utils;
+pub mod models;
+pub mod types;
+pub(crate) mod utils;
 
-use limiter_storage::memory::memory_store::MemoryStore;
-use limiter_storage::{self, store::QueryDatabase};
-
-use crate::limiter::RateLimiter;
-
-pub fn debug() {
-    let user_bucket = algorithms::token_bucket::TokenBucket::new_default_value();
-
-    let bucket_id: String = "1".to_string();
-    let db = MemoryStore::new();
-
-    // let bucket_status = models::BucketState::new(100, 1779535331);
-
-    // println!("bucket status is {:?}",bucket_status);
-    // db.create(bucket_id.clone(), bucket_status);
-
-    let result = user_bucket.check_rate(bucket_id.clone(), 5, &db);
-
-    println!("result of request is {}", result);
-    let bs = db.find(&bucket_id).unwrap();
-    println!("bucket after update => {:?}", bs);
-}
-
-// cargo run --example demo -p limiter-core
-
-// example of call memory in function
-// fn process_user_data<S>(store: &S, user_id: i32)
-// where
-//     S: Store<i32, UserProfile>
-// {
-//     if let Some(user) = store.get(&user_id) {
-//         println!("Processing: {}", user.username);
-//     }
-// }
